40269090cc07286eeff65df91735c428ba1ce0c8
umar-tech-git <umar9894.king@gmail.com>
2025-02-27 09:21:55 +0530

    expensesplitter

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import WelcomePage from "./components/WelcomePage";
 import ExpenseSplitter from "./components/ExpenseSplitter";
 import "./styles.css";
 function App() {
   return (
-    <Router>
-      <Routes>
+    <>
+    <Routes>
         <Route path="/" element={<WelcomePage />} />
         <Route path="/split" element={<ExpenseSplitter />} />
       </Routes>
-    </Router>
+    </>
+      
   );
 }
 
--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 
+
 const WelcomePage = () => {
   const navigate = useNavigate();
 
