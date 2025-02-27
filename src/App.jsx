b5d658d2d1594afeed94386c73cb2cf0499c2e32
umar-tech-git <umar9894.king@gmail.com>
2025-02-27 09:15:16 +0530

    expensesplitter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import WelcomePage from "./components/WelcomePage";
 import ExpenseSplitter from "./components/ExpenseSplitter";
-
+import "./styles.css";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
-        <Route path="/splitter" element={<ExpenseSplitter />} />
+        <Route path="/split" element={<ExpenseSplitter />} />
       </Routes>
     </Router>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import WelcomePage from "./components/WelcomePage";
-import ExpenseSplitter from "./components/ExpenseSplitter";
-import "./styles.css";
+import App from "./App";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/split" element={<ExpenseSplitter />} />
-      </Routes>
-    </Router>
+    <App/>
   </React.StrictMode>
 );
